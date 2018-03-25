1f7ba8010afa7e9d1f87f3a2ebc52da77e6fb358
sh-o <shog.ysd@gmail.com>
2018-03-25 14:09:27 +0900

    Update gitconfigscript.sh

--- gitconfigscript.sh
+++ gitconfigscript.sh
@@ -1,45 +1,57 @@
+# Time-stamp: <Sun Mar 25 14:08:57 JST 2018>
 #!/bin/bash
 
 if [ ${EUID:-${UID}} = 0 ]; then
-	echo "ERROR"
-	exit 1
+    echo "ERROR"
+    exit 1
 fi
 
 os=`uname 2>&1`
 if [ ${os} = "Darwin" ]; then
-	# macOS
-	download_command='curl -# -o'
+    # macOS
+    download_command='curl -# -o'
 else
-	# Linux ${os} = "Linux"
-	download_command='wget -O'
+    # Linux ${os} = "Linux"
+    download_command='wget -O'
 fi
 
-echo -n "gitignore_global ->"
-${download_command} ~/.gitignore_global https://raw.githubusercontent.com/shogysd/settings/master/gitignore_global 2>&1 | awk -F ' ' '{print $2}'
+echo "update file: .gitconfig"
+echo -n "start update?: y-or-n: "
+read start
+if [ $start = "y" ]; then
 
-rm ~/.gitconfig > /dev/null 2>&1
+    echo -n "gitignore_global ->"
+    ${download_command} ~/.gitignore_global https://raw.githubusercontent.com/shogysd/settings/master/gitignore_global 2>&1 | awk -F ' ' '{print $2}'
 
-git config --global core.excludesfile ~/.gitignore_global
-git config --global core.editor emacs
-git config --global core.pager less
-git config --global color.ui true
-git config --global core.autocrlf input
-git config --global push.default nothing
-# git config --global push.default simple
+    rm ~/.gitconfig > /dev/null 2>&1
 
-
-git config --global alias.co 'checkout'
-git config --global alias.st 'status'
-git config --global alias.br 'branch'
-git config --global alias.df 'diff'
-git config --global alias.clonea 'clone --recursive'
-git config --global alias.sub-clone 'submodule update --init --recursive'
-git config --global alias.wdiff 'diff --word-diff'
-git config --global alias.ignore-switch \!~/.gitignore-switch.bash
+    git config --global core.excludesfile ~/.gitignore_global
+    git config --global core.editor emacs
+    git config --global core.pager less
+    git config --global color.ui true
+    git config --global core.autocrlf input
+    git config --global push.default nothing
+    # git config --global push.default simple
 
 
-echo ""
-echo "When you execute the following command git's setup is completed!"
-echo "git config --global user.name '[username]'"
-echo "git config --global user.email '[useremail]'"
-echo ""
+    git config --global alias.co 'checkout'
+    git config --global alias.st 'status'
+    git config --global alias.br 'branch'
+    git config --global alias.df 'diff'
+    git config --global alias.clonea 'clone --recursive'
+    git config --global alias.sub-clone 'submodule update --init --recursive'
+    git config --global alias.wdiff 'diff --word-diff'
+    git config --global alias.ignore-switch \!~/.gitignore-switch.bash
+
+
+    echo ""
+    echo "When you execute the following command git's setup is completed!"
+    echo "git config --global user.name '[username]'"
+    echo "git config --global user.email '[useremail]'"
+    echo ""
+
+elif [ $start = "n" ]; then
+    echo "EXIT"
+else
+    echo "error"
+fi
