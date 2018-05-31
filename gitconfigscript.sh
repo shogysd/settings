6253f771c6cd369d22b7f5336f12816dd5925f67
sh-o <shog.ysd@gmail.com>
2018-05-31 21:52:15 +0900

    Update gitconfigscript.sh

--- gitconfigscript.sh
+++ gitconfigscript.sh
@@ -38,7 +38,7 @@ if [ $start = "y" ]; then
 
     git config --global alias.co 'checkout'
     git config --global alias.st 'status'
-    git config --global alias.br 'branch'
+    git config --global alias.br 'branch -v'
     git config --global alias.df 'diff'
     git config --global alias.clonea 'clone --recursive'
     git config --global alias.sub-clone 'submodule update --init --recursive'
