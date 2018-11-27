7da6ce74b32a00df0e115cbf83c6dddd906260da
sh-o <shog.ysd@gmail.com>
2018-11-28 00:43:35 +0900

    Update bash_script.bash

--- config_files/bash_script.bash
+++ config_files/bash_script.bash
@@ -179,6 +179,21 @@ function flush(){
 }
 
 
+function screen-clean(){
+    echo -n "cleanup detached screen?: y-or-n: "
+    read start
+    if [ $start = "y" ]; then
+        for id in `screen -ls | grep -e '(' -e ')' | tr '.' ' ' | grep 'Detached' | awk '{print $1}'`; do
+            screen -S ${id} -X quit
+        done
+    elif [ $start = "n" ]; then
+        echo "EXIT"
+    else
+        echo "error"
+    fi
+}
+
+
 function gd(){
     topLevelPath=`${git_command_path} rev-parse --show-toplevel 2> /dev/null`
     if  [ $? = 0 ]; then
