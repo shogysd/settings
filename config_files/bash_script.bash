6a86cf359f3d550d0f7f714dc662ca8af40a7cd8
sh-o <shog.ysd@gmail.com>
2018-12-01 20:30:52 +0900

    Update bash_script.bash

--- config_files/bash_script.bash
+++ config_files/bash_script.bash
@@ -315,8 +315,8 @@ function -screenPrinter(){
 
 
 function -screenSessionPrinter(){
-    screen -ls | grep Attached | tr '.' ' ' | echo -e "${esc_key}[1;32m`awk '{printf "%s %s\t(Attached)\n",  $1, $2}'`${esc_key}[0;39m"
-    screen -ls | grep Detached | tr '.' ' ' | awk '{printf "%s %s\t(Detached)\n",  $1, $2}'
+    screen -ls | grep Attached | tr '.' ' ' | echo -e "${esc_key}[1;32m`awk '{printf "%s %s\t(Attached)\n", $1, $2}'`${esc_key}[0;39m"
+    screen -ls | grep Detached | tr '.' ' ' | awk '{printf "%s %s\t(Detached)\n", $1, $2}'
 }
 
 
