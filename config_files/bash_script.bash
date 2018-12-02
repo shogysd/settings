7458f33c4c4d8a181764d2f1146918a32a286114
sh-o <shog.ysd@gmail.com>
2018-12-02 16:39:53 +0900

    Update bash_script.bash

--- config_files/bash_script.bash
+++ config_files/bash_script.bash
@@ -299,27 +299,29 @@ function -screenPrinter(){
         argument_for_adjustment='0'
     fi
 
-    screen -ls | grep "Attached" > /dev/null
+    screen -ls | grep -q "Attached"
     if [ $? = 0 ]; then
         screen -ls | grep "Attached" | grep `expr ${PPID} - ${argument_for_adjustment}` > /dev/null
         if [ $? = 0 ]; then
             screen -ls | grep "Attached" | grep `expr ${PPID} - ${argument_for_adjustment}` | tr '.' ' ' | awk '{printf "%s %s", $1, $2}'
             return 0
         else
-            for counter in `seq 1 3`;
-            do
-                predicted_id=`expr ${PPID} - ${argument_for_adjustment} - ${counter}`
-                screen -ls | grep "Attached" | grep ${predicted_id} > /dev/null
-                if [ $? = 0 ]; then
-                    screen -ls | grep ${pretected_id} | tr '.' ' ' | awk '{printf "%s %s", $1, $2}'
-                    echo -n " (estimated)"
-                    return 0
-                fi
-            done
+            # for counter in `seq 1 3`;
+            # do
+            #     predicted_id=`expr ${PPID} - ${argument_for_adjustment} - ${counter}`
+            #     screen -ls | grep "Attached" | grep ${predicted_id} > /dev/null
+            #     if [ $? = 0 ]; then
+            #         screen -ls | grep ${pretected_id} | tr '.' ' ' | awk '{printf "%s %s", $1, $2}'
+            #         echo -n " (estimated)"
+            #         return 0
+            #     fi
+            # done
             echo -ne "PPID: ${PPID}"
+            return 1
         fi
     else
         tty | cut -b 6-
+        return 0
     fi
 }
 
