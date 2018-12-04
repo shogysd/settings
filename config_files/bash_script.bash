3a08eaca6ff3928a5affbdcbbdf7882f167c05b6
sh-o <shog.ysd@gmail.com>
2018-12-05 00:28:16 +0900

    Update bash_script.bash

--- config_files/bash_script.bash
+++ config_files/bash_script.bash
@@ -49,15 +49,15 @@ function shis(){
     if [ $# = 1 ]; then
         history | \
             sed '$d' | \
-            awk 'match($0, / {,5}[^ ]{,5} {2}[^ ]{8} {1}[^ ]{10} {3}.*?'${1}'.*?/) { \
+            awk 'match($0, /.{5} {2}[^ ]{8} {1}[^ ]{10} {3}.*?'${1}'.*?/) { \
                     split($0, tmp, " "); \
                     printf "%5s  %8s %10s   ", tmp[1], tmp[2], tmp[3]; \
                     gsub("'${1}'", "\033[1;31m'${1}'\033[0;39m", $0); \
-                    match($0, / {,5}[^ ]+ {2}[^ ]+ {1}[^ ]+ {3}/); \
+                    match($0, /.{5} {2}[^ ]+? {1}[^ ]+? {3}/); \
                     print substr($0, RSTART+RLENGTH) \
                 }' | \
             tail -n 100
-            # 2>/dev/null
+        # 2>/dev/null
         return 0
     elif [ $# = 0 ]; then
         echo "error: nothing arguments"
