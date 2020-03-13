7ae410fb077708338206998128836d8d95347b58
sh-o <shog.ysd@gmail.com>
2020-03-14 00:18:11 +0900

    Update bash_script.bash

--- config_files/bash_script.bash
+++ config_files/bash_script.bash
@@ -78,25 +78,25 @@ function addpath(){
 }
 
 
-function shis(){                                                                                         
-    if [ $# = 1 ]; then                                                                                  
-        history | \                                                                                      
-        # sed '$d' | \                                                                                   
-        awk -F '   ' '{ \                                                                                
-            if ( $2 ~ /.*'${1}'.*/ ) { \                                                                 
-                gsub("'${1}'", "\033[1;31m'${1}'\033[0;39m", $2); \                                      
-                printf "%s   %s\n", $1, $2; \                                                            
-            } \                                                                                          
-        }'                                                                                               
-        # | tail -n 100 2>/dev/null                                                                      
-        return 0                                                                                         
-    elif [ $# = 0 ]; then                                                                                
-        echo "error: nothing arguments"                                                                  
-        return 1                                                                                         
-    else                                                                                                 
-        echo "error: many arguments"                                                                     
-        return 1                                                                                         
-    fi                                                                                                   
+function shis(){
+    if [ $# = 1 ]; then
+        history | \
+        # sed '$d' | \
+        awk -F '   ' '{ \
+            if ( $2 ~ /.*'${1}'.*/ ) { \
+                gsub("'${1}'", "\033[1;31m'${1}'\033[0;39m", $2); \
+                printf "%s   %s\n", $1, $2; \
+            } \
+        }'
+        # | tail -n 100 2>/dev/null
+        return 0
+    elif [ $# = 0 ]; then
+        echo "error: nothing arguments"
+        return 1
+    else
+        echo "error: many arguments"
+        return 1
+    fi
 }
 
 
